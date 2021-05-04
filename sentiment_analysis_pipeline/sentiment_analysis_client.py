cc00382655dd9fcb3b9a9c482b0843bcb02979cd
ddreakfordalgorithmia <ddreakford@algorithmia.io>
2021-05-04 13:36:34 -0400

    Reference v2.0.2 of the sentiment analysis pipeline.

--- sentiment_analysis_pipeline/sentiment_analysis_client.py
+++ sentiment_analysis_pipeline/sentiment_analysis_client.py
@@ -5,7 +5,7 @@ import json
 input = "data://algorithmia_se/SentimentAnalysisPipeline"
 
 client = Algorithmia.client(os.getenv('ALGORITHMIA_API_KEY'), os.getenv('ALGORITHMIA_API'))
-algo = client.algo('ddreakford/sentiment_analysis_pipeline/2.0.0')
+algo = client.algo('ddreakford/sentiment_analysis_pipeline/2.0.2')
 algo.set_options(timeout=300) # optional
 
 result = algo.pipe(input).result
