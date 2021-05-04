0a2580725800cd9bd16f47cf83e1fba84a75c201
ddreakfordalgorithmia <ddreakford@algorithmia.io>
2021-05-04 09:46:49 -0400

    Add sentiment analysis pipeline client.

---
+++ sentiment_analysis_pipeline/sentiment_analysis_client.py
@@ -0,0 +1,12 @@
+import Algorithmia
+import os
+import json
+
+input = "data://algorithmia_se/SentimentAnalysisPipeline"
+
+client = Algorithmia.client(os.getenv('ALGORITHMIA_API_KEY'), os.getenv('ALGORITHMIA_API'))
+algo = client.algo('ddreakford/sentiment_analysis_pipeline/2.0.0')
+algo.set_options(timeout=300) # optional
+
+result = algo.pipe(input).result
+print(json.dumps(result, indent=2))
